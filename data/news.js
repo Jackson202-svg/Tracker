1f170ad1082ba9e447d48977b8733eca19909913
Jackson202-svg <zwtwwkg65k@privaterelay.appleid.com>
2025-12-24 15:47:11 +0900

    Create news.js

---
+++ data/news.js
@@ -0,0 +1,9 @@
+const santaNews = [
+    "Rudolph's nose is shining at 100% brightness!",
+    "Cookie levels reaching critical lows in the sleigh.",
+    "Air Traffic Control in Tokyo has cleared Santa for landing.",
+    "Elf maintenance team reports the sleigh is in top condition.",
+    "Mrs. Claus has sent a 'Good Luck' text message.",
+    "Dashers speed reaching Mach 2 over the Pacific!",
+    "Radar detects a high concentration of gingerbread nearby."
+];
